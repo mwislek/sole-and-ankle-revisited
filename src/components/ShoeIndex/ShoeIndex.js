d6cc29a7a701676f921e1bcc4af8c8507e963940
M. Wislek <mike.wislek@gmail.com>
2023-03-16 20:19:28 -0500

    feat: add mobile index heacer

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { MEDIA_QUERY, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -24,6 +24,28 @@ const ShoeIndex = ({ sortId, setSortId }) => {
             <option value="price">Price</option>
           </Select>
         </Header>
+        <MobileHeader>
+          <TitleContainer>
+            <Breadcrumbs>
+              <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+              <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+              <Breadcrumbs.Crumb href="/sale/shoes">
+                Shoes
+              </Breadcrumbs.Crumb>
+            </Breadcrumbs>
+            <Title>Running</Title>
+          </TitleContainer>
+          <MobileSortContainer>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </MobileSortContainer>
+        </MobileHeader>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
@@ -51,6 +73,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${MEDIA_QUERY.LAPTOP_AND_SMALLER} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,7 +87,30 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
-`;
+
+  @media ${MEDIA_QUERY.LAPTOP_AND_SMALLER} {
+    display: none;
+  }
+`
+
+const MobileHeader = styled.header`
+  display: none;
+
+  @media ${MEDIA_QUERY.LAPTOP_AND_SMALLER} {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+`
+
+const MobileSortContainer = styled.div`
+  @media ${MEDIA_QUERY.PHONE_AND_SMALLER} {
+    display: none;
+  }
+`
+
+const TitleContainer = styled.div`
+`
 
 const Title = styled.h2`
   font-size: 1.5rem;
