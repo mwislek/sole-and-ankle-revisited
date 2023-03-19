e5d77659f8e718e213c820aa0ee9b2c1eea620e5
M. Wislek <mike.wislek@gmail.com>
2023-03-18 20:10:34 -0500

    feat: complete mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -7,30 +7,31 @@ import "@reach/dialog/styles.css";
 import UnstyledButton from "../UnstyledButton";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
+import { COLORS, WEIGHTS } from "../../constants";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
     <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <DialogContent>
-        <button onClick={onDismiss}>Dismiss menu</button>
-        <nav>
+      <Content aria-label="Menu">
+        <MenuButton onClick={onDismiss}>
+          <Icon id="close" strokeWidth={2} />
+          <VisuallyHidden>Close Mobile Menu</VisuallyHidden>
+        </MenuButton>
+        <Spacer />
+        <Navigation>
           <a href="/sale">Sale</a>
           <a href="/new">New&nbsp;Releases</a>
           <a href="/men">Men</a>
           <a href="/women">Women</a>
           <a href="/kids">Kids</a>
           <a href="/collections">Collections</a>
-        </nav>
-        <footer>
+        </Navigation>
+        <Footer>
           <a href="/terms">Terms and Conditions</a>
           <a href="/privacy">Privacy Policy</a>
           <a href="/contact">Contact Us</a>
-        </footer>
-      </DialogContent>
+        </Footer>
+      </Content>
     </Overlay>
   );
 };
@@ -42,7 +43,67 @@ const Overlay = styled(DialogOverlay)`
   left: 0;
   right: 0;
 
-  background-color: hsl(220 5% 40% / 0.8);
+  display: flex;
+  justify-content: flex-end;
+
+  background-color: hsl(220deg 5% 40% / 0.8);
 `
 
+const Content = styled(DialogContent)`
+  position: fixed;
+  top: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: column;
+
+  background: white;
+  height: 100%;
+  margin: 0%;
+  padding: 32px;
+  width: 300px;
+`
+
+const MenuButton = styled(UnstyledButton)`
+  position: fixed;
+  top: 10px;
+  right: 0;
+  padding: 16px;
+`
+
+const Navigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  & a {
+    color: ${COLORS.gray[900]};
+    font-size: ${18 / 16}rem;
+    font-weight: ${WEIGHTS.medium};
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+
+  & a:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: flex-end;
+
+  & a{
+    color: ${COLORS.gray[700]};
+    font-size: ${14 / 16}rem;
+    line-height: ${28 / 16}rem;
+    text-decoration: none;
+  }
+`
+
+const Spacer = styled.div`
+  flex: 1;
+`
 export default MobileMenu;
